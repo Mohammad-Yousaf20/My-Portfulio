e1add3e626271a9bd2ef23850386755d1ea5b5ba
Mohammad-Yousaf20 <mohammadyousafahmadzai0@gmail.com>
2025-12-11 02:51:57 -0800

    Add files via upload

---
+++ Portfuilo/script.js
@@ -0,0 +1,5 @@
+// Contact form alert
+document.getElementById("contactForm").addEventListener("submit", function(e){
+    e.preventDefault();
+    alert("Message sent successfully! I will contact you soon.");
+});
